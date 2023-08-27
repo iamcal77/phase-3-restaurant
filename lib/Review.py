633ce0e0800cfbe74ebb5770ada6e2f17c896b47
iamcal77 <calvinkipkirui99@gmail.com>
2023-08-27 13:31:45 +0300

    customer.py updated

---
+++ lib/Review.py
@@ -0,0 +1,23 @@
+class Review:
+    all_reviews = []
+
+    def __init__(self, customer, restaurant, rating):
+        self._customer = customer
+        self._restaurant = restaurant
+        self._rating = rating
+        Review.all_reviews.append(self)
+        customer.add_review(self)
+        restaurant.add_review(self)
+
+    def rating(self):
+        return self._rating
+
+    def customer(self):
+        return self._customer
+
+    def restaurant(self):
+        return self._restaurant
+
+    @classmethod
+    def all(cls):
+        return cls.all_reviews
